3fc6918bd6073ffc903d5e40cb4e766186afea95
Paguiel <119151331+paguielnganji@users.noreply.github.com>
2023-09-05 22:02:54 +0200

    ExampleInstrumentedTest.kt

--- ExampleInstrumentedTest.kt
+++ ExampleInstrumentedTest.kt
@@ -1,45 +1,24 @@
-<resources>
-    <string name="app_name" translatable="false">Nature Emoi</string>
+package fr.paguiel.naturecollection
 
-    <!-- Page d'acceuil -->
-    <string name="home_page_title" translatable="false">A découvrir</string>
-    <string name="home_page_plant_section" translatable="false">Dernières plantes</string>
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
 
-    <!-- Page Collection -->
-    <string name="collection_page_title" translatable="false">Ma collection</string>
+import org.junit.Test
+import org.junit.runner.RunWith
 
-    <!-- Page d'Ajout de plantes -->
-    <string name="add_plant_page_title" translatable="false">Nom de la plante</string>
-    <string name="add_plant_page_upload_button" translatable="false">Charger Image</string>
-    <string name="add_plant_name_input" translatable="false">@string/item_plant_name</string>
-    <string name="add_plant_page_description_input" translatable="false">@string/item_plant_description</string>
+import org.junit.Assert.*
 
-    <string-array name="add_plant_grow_spinner_input">
-        <item>Croissance : Lente</item>
-        <item>Croissance : Moyenne</item>
-        <item>Croissance : Rapide</item>
-    </string-array>
-
-    <string-array name="add_plant_water_spinner_input">
-        <item>Consommation : Faible</item>
-        <item>Consommation : Moyenne</item>
-        <item>Consommation : Elevée</item>
-    </string-array>
-
-    <string name="add_plant_page_confirm_button">Confirmer</string>
-
-    <!--Fenetre Popup de details -->
-    <string name="popup_plant_name">Plant Name</string>
-    <string name="popup_plant_description_title" translatable="false">Description</string>
-    <string name="popup_plant_description_subtitle" translatable="false">Sous-description</string>
-    <string name="popup_plant_grow_title" translatable="false">Croissance</string>
-    <string name="popup_plant_grow_subtitle" translatable="false">Lente</string>
-    <string name="popup_plant_water_title" translatable="false">Consomation</string>
-    <string name="popup_plant_water_subtitle" translatable="false">Faible</string>
-
-    <!-- Plante Customize -->
-    <string name="item_plant_name" translatable="false">Nom de la plante</string>
-    <string name="item_plant_description" translatable="false">Description de la plante</string>
-
-
-</resource>
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("fr.paguiel.naturecollection", appContext.packageName)
+    }
+}
